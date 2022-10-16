bde8a94aee128d1266f4e7e633df4ea122357192
Akshay Nair <phenax5@gmail.com>
2022-10-16 16:59:35 +0530

    feat: adds top level fields autocompletion

--- lua/cmp-gql/init.lua
+++ lua/cmp-gql/init.lua
@@ -5,6 +5,7 @@ local source = {}
 
 function source.new()
   local self = setmetatable({}, { __index = source })
+  self._schema = nil
   return self
 end
 
@@ -30,26 +31,58 @@ local function get_operation_type(node, bufnr)
   return get_operation_type(node:parent(), bufnr)
 end
 
+function source._get_schema(self)
+  if self._schema ~= nil then return self._schema end
+  
+  local file_path = vim.loop.cwd() .. '/graphql.schema.json'
+
+  local fd = assert(vim.loop.fs_open(file_path, 'r', 438))
+  local stat = assert(vim.loop.fs_stat(file_path))
+  local contents = assert(vim.loop.fs_read(fd, stat.size, 0))
+  local schema = vim.json.decode(contents).__schema
+  self._schema = schema
+
+  return schema
+end
+
 ---@param params cmp.SourceCompletionApiParams
 ---@param callback fun(response: lsp.CompletionResponse|nil)
 function source.complete(self, params, callback)
-  -- get current node
-  -- if selection_set,
-  --    check the ancestor -> operation_definition
-  --      -> operation_definition.operation_type -> mutation, fields inside mutation type
-  --      -> field -> 
   vim.defer_fn(function()
     local bufnr = vim.fn.bufnr('%')
     local node = util.get_ts_node_under_cursor()
     local op_type = get_operation_type(node, bufnr)
+    local schema = self:_get_schema()
+    local op_type_name = schema[op_type .. 'Type'].name
+
+    if node:parent():type() == "operation_definition" then
+      local matching_fields = {}
+      for _, ty in pairs(schema.types) do
+        if ty.kind == 'OBJECT' and ty.name == op_type_name then
+          for _, field in pairs(ty.fields) do
+            local item = {
+              label = field.name,
+              kind = cmp_lsp.CompletionItemKind.Field,
+              insertText = field.name,
+              detail = "field",
+              documentation = field.description,
+            }
+            table.insert(matching_fields, item)
+          end
+        end
+      end
+
+      callback(matching_fields)
+      return
+    end
 
     callback({
       {
-        label = 'january',
+        label = 'january:' .. op_type,
         kind = cmp_lsp.CompletionItemKind.Field,
         insertText = 'januaryAsAField',
+        detail = op_type,
         documentation = 'This is the fields description',
-        detail = 'This is a field',
       },
     })
   end, 0)
